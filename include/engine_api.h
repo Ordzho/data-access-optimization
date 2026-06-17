d7097fe4b2b7e79da802262725d17c2d134e3a5c
Human <42119715+Ordzho@users.noreply.github.com>
2026-06-18 02:29:36 +0700

    Update engine_api.h

--- include/engine_api.h
+++ include/engine_api.h
@@ -1,1 +1,9 @@
+#ifndef ENGINE_API_H
+#define ENGINE_API_H
 
+// Только то, что нужно знать пользователю
+void init_system();
+void run_sync_cycle();
+void update_state(int id, int value);
+
+#endif
